9d5ab25c46d7e9b229d003af3a52dd0460f6e618
Alain Schlesser <alain.schlesser@gmail.com>
2018-05-22 16:32:12 +0200

    Adapt package to turn it into a reusable testing framework

--- features/extra/no-mail.php
+++ features/extra/no-mail.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * This file is copied as amu-plugin into new WP installs to reroute normal
+ * mails into log entries.
+ */
+
 function wp_mail( $to ) {
 	// Log for testing purposes
 	WP_CLI::log( "WP-CLI test suite: Sent email to {$to}." );
