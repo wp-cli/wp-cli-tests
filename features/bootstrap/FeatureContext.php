04c44739c3a662663c5bff58a98b646b87d0d0e2
Alain Schlesser <alain.schlesser@gmail.com>
2018-08-03 15:54:02 +0200

    Second try at adding fold sections

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -244,16 +244,18 @@ class FeatureContext extends BehatContext implements ClosuredContextInterface {
 			self::log_run_times_before_suite( $event );
 		}
 
+		$travis = getenv( 'TRAVIS' );
+
 		$result = Process::create( 'wp cli info', null, self::get_process_env_variables() )->run_check();
 		echo PHP_EOL;
 		echo $result->stdout;
 		echo PHP_EOL;
 		self::cache_wp_files();
 		$result = Process::create( Utils\esc_cmd( 'wp core version --debug --path=%s', self::$cache_dir ), null, self::get_process_env_variables() )->run_check();
-		Process::create( 'echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"', null, self::get_process_env_variables() )->run_check();
+		$travis && print( "travis_fold:start:wp_cli_debug\n" );
 		echo "[Debug messages]\n";
 		echo $result->stderr;
-		Process::create( 'echo -e "\ntravis_fold:end:$1\r"', null, self::get_process_env_variables() )->run_check();
+		$travis && print( "travis_fold:end:wp_cli_debug\n" );
 		echo PHP_EOL;
 		echo 'WordPress ' . $result->stdout;
 
