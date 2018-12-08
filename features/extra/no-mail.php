e13f0f535c7b4bce56a34d38b8795d87dd6f6bfe
jrfnl <jrfnl@users.noreply.github.com>
2018-12-08 06:04:43 +0100

    PHPCS: whitelist some code for select sniffs
    Includes minor documentation fix.

--- features/extra/no-mail.php
+++ features/extra/no-mail.php
@@ -1,12 +1,18 @@
 <?php
-
-/*
+/**
  * This file is copied as amu-plugin into new WP installs to reroute normal
  * mails into log entries.
  */
 
+/**
+ * Replace WP native pluggable wp_mail function for test purposes.
+ *
+ * @param string $to Email address.
+ *
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- WP native function.
+ */
 function wp_mail( $to ) {
 	// Log for testing purposes
 	WP_CLI::log( "WP-CLI test suite: Sent email to {$to}." );
 }
-
+// phpcs:enable
