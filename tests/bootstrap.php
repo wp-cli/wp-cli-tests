eaf4fe25a337cdcba00a3a8aa5817a774335d6c0
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-18 15:01:12 +0200

    Wrap PHPUnit bootstrapping code into a function

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,26 +33,32 @@ if ( class_exists( 'PHPUnit\Runner\Version' ) ) {
 require_once VENDOR_DIR . '/autoload.php';
 require_once WP_CLI_ROOT . '/php/utils.php';
 
-$config_filenames = array(
-	'phpunit.xml',
-	'.phpunit.xml',
-	'phpunit.xml.dist',
-	'.phpunit.xml.dist',
+function wpcli_tests_include_config( array $config_filenames = [] ) {
+	$config_filename = false;
+	foreach ( $config_filenames as $filename ) {
+		if ( file_exists( PACKAGE_ROOT . '/' . $filename ) ) {
+			$config_filename = PACKAGE_ROOT . '/' . $filename;
+			break;
+		}
+	}
+
+	if ( $config_filename ) {
+		$config  = file_get_contents( $config_filename );
+		$matches = null;
+		$pattern = '/bootstrap="(?P<bootstrap>.*)"/';
+		$result  = preg_match( $pattern, $config, $matches );
+		if ( isset( $matches['bootstrap'] ) && file_exists( $matches['bootstrap'] ) ) {
+			include_once PACKAGE_ROOT . '/' . $matches['bootstrap'];
+		}
+	}
+}
+
+wpcli_tests_include_config(
+	[
+		'phpunit.xml',
+		'.phpunit.xml',
+		'phpunit.xml.dist',
+		'.phpunit.xml.dist',
+	]
 );
 
-$config_filename = false;
-foreach ( $config_filenames as $filename ) {
-	if ( file_exists( PACKAGE_ROOT . '/' . $filename ) ) {
-		$config_filename = PACKAGE_ROOT . '/' . $filename;
-	}
-}
-
-if ( $config_filename ) {
-	$config  = file_get_contents( $config_filename );
-	$matches = null;
-	$pattern = '/bootstrap="(?P<bootstrap>.*)"/';
-	$result  = preg_match( $pattern, $config, $matches );
-	if ( isset( $matches['bootstrap'] ) && file_exists( $matches['bootstrap'] ) ) {
-		include_once PACKAGE_ROOT . '/' . $matches['bootstrap'];
-	}
-}
