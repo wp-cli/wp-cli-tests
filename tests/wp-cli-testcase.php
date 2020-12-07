3998173f347ab87fdd8458ee92aee0533c8d824f
Alain Schlesser <alain.schlesser@gmail.com>
2020-12-07 21:39:46 +0000

    Add centralized test case with polyfills

---
+++ tests/wp-cli-testcase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WP_CLI\Tests;
+
+use Yoast\PHPUnitPolyfills\TestCases\TestCase as PolyfilledTestCase;
+
+/**
+ * WP-CLI base test case.
+ */
+class TestCase extends PolyfilledTestCase {
+
+}
