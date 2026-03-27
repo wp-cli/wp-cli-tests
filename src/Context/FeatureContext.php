6b5c279907937693b3782ca449fca4967d8ec6e7
Pascal Birchler <pascalb@google.com>
2026-03-28 00:08:34 +0100

    mkdir tweak

--- src/Context/FeatureContext.php
+++ src/Context/FeatureContext.php
@@ -1188,9 +1188,10 @@ class FeatureContext implements Context {
 	 */
 	public function create_run_dir(): void {
 		if ( ! isset( $this->variables['RUN_DIR'] ) ) {
-			self::$run_dir              = realpath( sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid( 'wp-cli-test-run-' . self::$temp_dir_infix . '-', true ) );
+			$temp_run_dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . uniqid( 'wp-cli-test-run-' . self::$temp_dir_infix . '-', true );
+			mkdir( $temp_run_dir );
+			self::$run_dir              = realpath( $temp_run_dir );
 			$this->variables['RUN_DIR'] = self::$run_dir;
-			mkdir( $this->variables['RUN_DIR'] );
 		}
 	}
 
